7d89e62e026338c3aa6ee5883ba597dcea7ad57d
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-04 10:22:00 +0530

    Initial Set-up

---
+++ src/com/rakuten/SavingsRepo.java
@@ -0,0 +1,8 @@
+package com.rakuten;
+
+public class SavingsRepo {
+
+	public String credit(float amount) {
+		return "Salary is Credited "+amount;
+	}
+}
---
+++ src/com/rakuten/Service.java
@@ -0,0 +1,10 @@
+package com.rakuten;
+
+public class Service {
+	private Account account;
+	
+	void setAccount(Account account) {
+		this.account=account;
+	}
+
+}

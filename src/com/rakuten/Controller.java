18b76beccf82365b8b6715ebda48d40dcbbc681c
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-04 11:11:04 +0530

    Set up beans

--- src/com/rakuten/Controller.java
+++ src/com/rakuten/Controller.java
@@ -1,5 +1,16 @@
 package com.rakuten;
 
-public class Controller {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+public class Controller {
+	public static void main(String[] args) {
+		System.out.println("Initializing Spring Container .....");
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("Bean.xml");
+		Service service=
+				(Service)ctx.getBean("service");
+		System.out.println(service.creditService(100000));
+	
+}
 }
--- src/com/rakuten/SavingsRepo.java
+++ src/com/rakuten/SavingsRepo.java
@@ -1,7 +1,15 @@
 package com.rakuten;
 
-public class SavingsRepo {
+import org.springframework.stereotype.Component;
 
+@Component
+public class SavingsRepo implements Account {
+
+	public SavingsRepo() {
+		System.out.println("Savings Constructor..");
+		// TODO Auto-generated constructor stub
+	}
+	
 	public String credit(float amount) {
 		return "Salary is Credited "+amount;
 	}
